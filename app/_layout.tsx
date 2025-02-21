8b1c73d59f3228144d8d7fa8000eee1722e255f6
SofienRogue <sofien.abidi@digitalroguewave.com>
2025-02-21 10:25:34 +0100

    add user details page 1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import {useEffect, useState} from 'react';
@@ -12,69 +12,75 @@ import { useAppState } from "@/hooks/useAppState";
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import sessionStore from "@/store/sessionStore";
-import Intro from "@/app/intro";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [showIntro, setShowIntro] = useState<boolean>(false);
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+const InitialLayout = () => {
+    const router = useRouter();
+    const [showIntro, setShowIntro] = useState<boolean>(true);
 
-  const queryClient = new QueryClient({
-    defaultOptions: { queries: { retry: 2 } },
-  });
+    useEffect(() => {
+        if (showIntro) {
+            router.replace('/details');
+        } else {
+            router.replace('/auth/register');
+        }
+    }, []);
 
-  // Use the custom hooks
-  useOnlineManager();
-  useAppState();
-
-  useEffect(() => {
-    // Check if the session is loaded and if the intro should be shown
-    const session = sessionStore.getState();
-    if (!session.token) {
-      setShowIntro(false);
-    }
-  }, []);
-
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  if (showIntro) {
-    return <Intro onFinish={() => setShowIntro(false)} />;
-  }
-
-  return (
-    <GluestackUIProvider mode="light">
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <QueryClientProvider client={queryClient}>
-          <Stack
+    return (
+        <Stack
             screenOptions={{
-              headerShown: false,
-              animation: 'none',
-              orientation: 'portrait',
-              navigationBarHidden: true,
-              statusBarHidden: true,
+                headerShown: false,
+                animation: 'slide_from_bottom',
+                orientation: 'portrait',
+                navigationBarHidden: true,
+                statusBarHidden: true,
             }}
-          >
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" />
-          </Stack>
-          <StatusBar style="auto" />
-        </QueryClientProvider>
-      </ThemeProvider>
-    </GluestackUIProvider>
-  );
+        >
+            <Stack.Screen name="(root)" options={{ headerShown: false }} />
+            <Stack.Screen name="auth/register" options={{ headerShown: false, animation: 'slide_from_right' }} />
+            <Stack.Screen name="auth/login" options={{ headerShown: false }} />
+            <Stack.Screen name="intro" options={{ headerShown: false }} />
+            <Stack.Screen name="details" options={{ headerShown: false }} />
+        </Stack>
+    );
+};
+
+export default function ProjectLayout() {
+    const colorScheme = useColorScheme();
+
+    const [loaded] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    });
+
+    const queryClient = new QueryClient({
+        defaultOptions: { queries: { retry: 2 } },
+    });
+
+    // Use the custom hooks
+    useOnlineManager();
+    useAppState();
+
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
+    }
+
+    return (
+        <GluestackUIProvider mode="light">
+            <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+                <QueryClientProvider client={queryClient}>
+                    <InitialLayout />
+                    <StatusBar style="auto" />
+                </QueryClientProvider>
+            </ThemeProvider>
+        </GluestackUIProvider>
+    );
 }
