aaefb96ac8d1392d311231ec8b7a4251763926b9
SofienRogue <sofien.abidi@digitalroguewave.com>
2025-02-23 13:55:21 +0100

    example tanstack query usage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,16 +2,21 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { useFonts } from 'expo-font';
-import {Stack, useRouter} from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
 import { useOnlineManager } from "@/hooks/useOnlineManager";
 import { useAppState } from "@/hooks/useAppState";
 
 import { useColorScheme } from '@/hooks/useColorScheme';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
+import { useReactQueryDevTools } from '@dev-plugins/react-query';
+import { View } from "react-native";
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+import ErrorBoundary from "react-native-error-boundary";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -19,9 +24,21 @@ const InitialLayout = () => {
     const router = useRouter();
     const [showIntro] = useState<boolean>(true);
 
+    const user = useQuery({
+        queryKey: ['user'],
+        queryFn: () => {
+            return {
+                status: 200,
+                data: {
+                    result: "tanstack init succeded"
+                }
+            }
+        },
+    });
+    console.log("user", user);
     useEffect(() => {
         if (showIntro) {
-            router.replace('/plans');
+            router.replace('/details');
         } else {
             router.replace('/register');
         }
@@ -54,6 +71,8 @@ export default function ProjectLayout() {
     const queryClient = new QueryClient({
         defaultOptions: { queries: { retry: 2 } },
     });
+    // Enable Tanstack query dev tools
+    useReactQueryDevTools(queryClient);
 
     // Use the custom hooks
     useOnlineManager();
@@ -70,12 +89,22 @@ export default function ProjectLayout() {
         return null;
     }
 
+    const ErrorFallback = ({ error, resetError }: any) => (
+        <View>
+            <Text>Oops! Something went wrong:</Text>
+            <Text>{error.toString()}</Text>
+            <Button onPress={resetError}>Reset</Button>
+        </View>
+    );
+
     return (
         <GluestackUIProvider mode="light">
             <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
                 <QueryClientProvider client={queryClient}>
-                    <InitialLayout />
-                    <StatusBar style="auto" />
+                    <ErrorBoundary FallbackComponent={ErrorFallback}>
+                        <InitialLayout />
+                        <StatusBar style="auto" hidden={true} />
+                    </ErrorBoundary>
                 </QueryClientProvider>
             </ThemeProvider>
         </GluestackUIProvider>
